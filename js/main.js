5f9a988f10f955b8eb91afcbbae05ea11c678654
Paul Potnuru <paul411@gmail.com>
2015-08-12 21:45:52 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 var app = {
+	
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
 
     findByName: function() {
         console.log('findByName');
@@ -14,9 +22,14 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+		var self = this;
+		this.store = new MemoryStore(function() {
+			self.showAlert('Store Initialized', 'Info');
+		});
+		
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+}
+	
 
 };
 
